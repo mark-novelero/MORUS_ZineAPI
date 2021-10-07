a8e96fd02a5670518017f46c081d3a04e956925b
mark novelero <mark.novelero@gmail.com>
2021-10-07 16:30:01 -0400

    MongoDB Connected

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async() => {
+
+    try{
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });            // this will return a promise
+        console.log('Mongo connected!');
+    } catch(err) {
+        console.log(err.message);              // if there is an error, let the process fail
+        process.exit(1); 
+    }
+}
+
+ module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express'); 
+const connectDB = require('./config/db')
 
 const app = express(); 
+
+connectDB(); //connect database
 
 app.get('/', (req, res) => res.send('API Running'))
 
