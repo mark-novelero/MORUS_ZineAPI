031228e2fb99b5fe27be05f8716247e7873d6419
mark novelero <mark.novelero@gmail.com>
2021-10-10 09:07:00 -0400

    Username and password length validation complete

--- Routes/API/users.js
+++ Routes/API/users.js
@@ -6,8 +6,17 @@ const { check, validationResult } = require('express-validator/check');
 // @desc    Test route
 // @access  Public 
 
-router.post('/', (req, res) => {
-   console.log(req.body); 
+router.post('/', [
+    check('username', 'username is required')
+    .not()
+    .isEmpty(), 
+    check('password', 'Please enter a password with 6 or more characters')
+    .isLength({ min: 6})
+], (req, res) => {
+   const errors = validationResult(req); 
+   if(!errors.isEmpty()) {
+       return res.status(400).json({errors: errors.array()})
+   } 
    res.send('User route');
 })
 
