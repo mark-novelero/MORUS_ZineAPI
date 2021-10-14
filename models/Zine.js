6941cec3ddb4fc08ced105531c96ef8620eb1ea9
mark novelero <mark.novelero@gmail.com>
2021-10-14 17:11:15 -0400

    zine schema complete

---
+++ models/Zine.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose'); 
+
+const ZineSchema = new mongoose.Schema({
+    title: {
+        type: String, 
+        required: true
+    },
+    creator: {
+        type: String, 
+        requires: true
+    }, 
+    pdf_url: {
+        type: String, 
+        required: true
+    }, 
+    indentifier: {
+        type: Number, 
+        required: true
+    }, 
+    content_description: {
+        type: String, 
+        required: true
+    }, 
+    subject: {
+        type: String, 
+        required: true
+    }, 
+    genre: {
+        type: String, 
+        required: true
+    }, 
+    publisher: {
+        type: String, 
+        required: true
+    }, 
+    date: {
+        type: Number, 
+        min: 0, 
+        required: true
+    }, 
+    format: {
+        type: String, 
+        required: true
+    }, 
+    source: {
+        type: String, 
+        required: true
+    }, 
+    language: {
+        type: String, 
+        required: true, 
+    },
+    relation: {
+        type: String, 
+        required: true
+    }, 
+    coverage: {
+        type: String, 
+        required: true
+    }, 
+    rights: {
+        type: String, 
+        required: true
+    },
+    cover_page_url: {
+        type: String, 
+        required: true
+    }  
+})
+
+module.exports = Zine = mongoose.model('zine', ZineSchema);
