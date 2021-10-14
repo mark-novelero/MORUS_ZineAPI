51a0ce18ac510edb09c7960388d482a5d3a48b89
mark novelero <mark.novelero@gmail.com>
2021-10-14 17:27:51 -0400

    get request all zines

--- Routes/API/zines.js
+++ Routes/API/zines.js
@@ -1,9 +1,20 @@
 const express = require('express'); 
 const router = express.Router(); 
+const auth = require('../../middleware/auth'); 
+const Zine = require('../../models/Profile'); 
+
 
 // @route   GET api/zines
-// @desc    Test route
+// @desc    get all zines
 // @access  Public 
-router.get('/', (req, res) => res.send('Zines Route'))
+router.get('/', auth, async (req, res) => {                               //add auth to make it a protected route
+
+    try{
+                                                                        // wait till next video
+    } catch(err){
+        console.log(err.message); 
+        res.status(500).send('Server Error')
+    }
+})        
 
 module.exports = router; 
